b87180e63f8805141656cd9d7948231b5a6ad500
Oscar Otero <oom@oscarotero.com>
2013-01-31 17:58:51 +0100

    Cada plugin vai nun arquivo distinto

---
+++ tabs.js
@@ -0,0 +1,37 @@
+/**
+ * Tabs
+ * [version 1.0]
+ */
+
+(function () {
+	jQuery.fn.tabs = function () {
+		return this.each(function () {
+			var $this = $(this),
+				$tabs = $this.find('> ul a'),
+				$contents = $this.find('> :not(ul)').hide();
+
+			$tabs.on('click', function () {
+				var $this = $(this),
+					id = $this.attr('href');
+
+				$contents.not(id).hide().trigger('tabHide');
+				$contents.filter(id).show().trigger('tabShow');
+				$tabs.removeClass('active');
+				$this.addClass('active');
+
+				return false;
+			});
+
+			var $tab;
+
+			if (location.hash) {
+				$tab = $tabs.filter('[href=' + location.hash + ']');
+			}
+			if (!$tab || !$tab.length) {
+				$tab = $tabs.first();
+			}
+
+			$tab.click();
+		});
+	}
+})();
