5e05efabd53a494247fb6d9a02d29d1befa794ee
Oscar Otero <oom@oscarotero.com>
2013-08-06 16:34:01 +0200

    Nova versión do plugin tabs, usando o jquery.boilerplate como base

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,40 +1,81 @@
 /**
  * Tabs
- * [version 1.1]
+ * [version 2.0]
  */
+;(function ($, window, document, undefined) {
+	var pluginName = "tabs", defaults = {};
 
-(function () {
-	jQuery.fn.tabs = function () {
-		return this.each(function () {
-			var $this = $(this),
-				$tabs = $this.find('> ul a'),
-				$contents = $this.find('> :not(ul)').hide();
+	function Plugin (element, options) {
+		this.element = element;
+		this.settings = $.extend({}, defaults, options);
 
-			$tabs.on('click', function () {
-				var $this = $(this),
-					id = $this.attr('href');
+		this.init();
+	}
 
-				$contents.not(id).hide().trigger('tabHide');
-				$contents.filter(id).show().trigger('tabShow');
-				$tabs.removeClass('active');
+	Plugin.prototype = {
+		init: function () {
+			this.$element = $(this.element);
+			this.$tabs = this.$element.find('> ul a');
+			this.$contents = this.$element.find('> :not(ul)').hide();
+
+			var that = this;
+
+			this.$tabs.on('click.tabs', function () {
+				var $this = $(this), id = $this.attr('href');
+
+				that.$contents.not(id).hide().trigger('tabHide');
+				that.$contents.filter(id).show().trigger('tabShow');
+				that.$tabs.removeClass('active');
 				$this.addClass('active');
 
 				return false;
 			});
 
-			var $tab = $tabs.filter('.active');
+			var $tab = this.$tabs.filter('.active');
 
 			if (!$tab.length) {
 				if (location.hash) {
-					$tab = $tabs.filter('[href=' + location.hash + ']');
+					$tab = this.$tabs.filter('[href=' + location.hash + ']');
 				}
 
 				if (!$tab || !$tab.length) {
-					$tab = $tabs.first();
+					$tab = this.$tabs.first();
 				}
 			}
 
 			$tab.click();
-		});
-	}
-})();
+		},
+		destroy: function () {
+			this.$tabs.off('.tabs');
+			this.$contents.off('tabHide tabShow');
+		}
+	};
+
+	$.fn[pluginName] = function (options) {
+		if ((options === undefined) || (typeof options === 'object')) {
+			return this.each(function () {
+				if (!$.data(this, "plugin_" + pluginName)) {
+					$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+				}
+			});
+		}
+
+		if ((typeof options === 'string') && (options[0] !== '_') && (options !== 'init')) {
+			var returns, args = arguments;
+
+			this.each(function () {
+				var instance = $.data(this, 'plugin_' + pluginName);
+
+				if ((instance instanceof Plugin) && (typeof instance[options] === 'function')) {
+					returns = instance[options].apply(instance, Array.prototype.slice.call(args, 1));
+				}
+
+				if (options === 'destroy') {
+				  $.data(this, 'plugin_' + pluginName, null);
+				}
+			});
+
+			return returns !== undefined ? returns : this;
+		}
+	};
+})(jQuery, window, document);
